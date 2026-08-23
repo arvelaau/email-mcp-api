1f6a3352c9c6a82ca8a366a1dcf5ad5470141fbf
arvelaau <awntariksa@gmail.com>
2026-08-23 22:07:09 +0700

    Update check_mail.js

--- api/check_mail.js
+++ api/check_mail.js
@@ -3,6 +3,7 @@ const { simpleParser } = require('mailparser');
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
